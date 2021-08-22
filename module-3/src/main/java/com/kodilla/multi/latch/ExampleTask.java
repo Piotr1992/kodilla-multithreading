2415dfde7111610bf00b0a1a1e669a15f5b2fe81
Lenovo <szyperek.piotr@o2.pl>
2021-08-22 18:12:52 +0200

    Task 3.3. thread synchronization with a latch.

---
+++ module-3/src/main/java/com/kodilla/multi/latch/ExampleTask.java
@@ -0,0 +1,33 @@
+package com.kodilla.multi.latch;
+
+import java.time.Instant;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExampleTask implements Runnable {
+
+    private CountDownLatch theLatch;
+    private Instant someTime;
+
+    public ExampleTask(CountDownLatch theLatch) {
+        this.theLatch = theLatch;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+
+        for (int n = 0; n < 5; n++) {
+            someTime = Instant.ofEpochMilli(
+                    ThreadLocalRandom.current().nextLong(500, 2000)
+            );
+            doTask();
+        }
+    }
+
+    private void doTask() {
+        System.out.println("Running the task");
+        theLatch.countDown();
+        System.out.println("someTime = " + someTime);
+    }
+}
